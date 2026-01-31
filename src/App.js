6e82a913944f185abdb4b8b3a8b0cfbb71ee9b97
Chukwuemeka Torti <emekatorti@gmail.com>
2026-01-31 18:54:22 -0500

    first component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
+import Employee from "./components/Employee";
 
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello, World!</h1>
-     
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <Employee />
+      </main>
     </div>
   );
 }
