2cdb89413fe16d49d255996782bc744d6dc046d3
Chukwuemeka Torti <emekatorti@gmail.com>
2026-01-31 19:05:15 -0500

    first prop

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import Employee from "./components/Employee";
 
 
 function App() {
+  const showEmployees = true;
   return (
     <div className="App">
       <main>
-        <Employee />
+        {showEmployees ? (
+          <>
+        <Employee name="Alice" />
+        <Employee name="Bob" />
+        <Employee name="Charlie" />
+        <Employee name="Diana" />
+        <Employee name="Ethan" /> 
+        </>):
+        <h3>No employees to show</h3>
+        }
       </main>
     </div>
   );
